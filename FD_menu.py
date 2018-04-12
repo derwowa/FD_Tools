a1c41ff5d596430c90410c8989d371fcc08860b8
wowa <>
2018-04-12 14:13:06 +0200

    edited path to be compatible with macOS

--- FD_menu.py
+++ FD_menu.py
@@ -16,7 +16,7 @@ class fdMenu(bpy.types.Menu):
     
     global arr_scripts, fd_scriptPath
     if os.name == "posix":
-        fd_basePath="/_Daten/OneDrive/Blender/Plugins/FD_Menu"
+        fd_basePath="/_Daten/OneDrive/Blender/Git Repository/FD Tools"
         fd_scriptPath=fd_basePath+"/scripts"
         exec(compile(open(fd_basePath+"/fd_runScriptOperator.py").read(), fd_basePath+"fd_runScriptOperator.py", 'exec'))
     else:
